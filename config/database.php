26029fa7cc21ce2c1f6db7c432633efc110d346c
ripasfilqadar <rif2602@gmail.com>
2015-09-22 22:59:47 +0700

    update

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'travelaja'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', ''),
+            'password'  => env('DB_PASSWORD', 'T4k0kn0R1p45'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
